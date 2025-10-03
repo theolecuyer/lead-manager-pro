27bea9a79f69bff2cb97e5f1eff2f9948140698e
Theo Lecuyer <theoglecuyer@gmail.com>
2025-10-03 16:37:51 -0700

    feat(dashboard) dashboard component + scaffolding

---
+++ src/app/admin/components/DashboardIcon.tsx
@@ -0,0 +1,63 @@
+import { ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/24/solid"
+
+export type DashboardIconProps = {
+	icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
+	textcolor: string
+	color1: string
+	color2: string
+	numToday: number
+	numYesterday: number
+	title: string
+}
+
+type Difference = {
+	icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
+	percentage: number
+	sign: string
+	color: string
+}
+
+const calculateDiff = (numToday: number, numYesterday: number): Difference => {
+	const diff = ((numToday - numYesterday) / (numToday + numYesterday)) * 100
+	if (diff >= 0) {
+		return { icon: ArrowUpIcon, percentage: diff, sign: "+", color: "text-green-500" }
+	} else {
+		return { icon: ArrowDownIcon, percentage: diff, sign: "-", color: "text-red-500" }
+	}
+}
+
+export default function DashboardIcon(dashboardIconProps: DashboardIconProps) {
+	const diffObject = calculateDiff(dashboardIconProps.numToday, dashboardIconProps.numYesterday)
+
+	return (
+		<div className="bg-white p-5 rounded shadow grid grid-cols-4 ">
+			<div className="col-span-3">
+				<p className="text-xs sm:text-sm font-light font-sans whitespace-nowrap">
+					{dashboardIconProps.title}
+				</p>
+				<p
+					className={`font-extrabold ${dashboardIconProps.textcolor} text-3xl mt-2 font-sans`}
+				>
+					{dashboardIconProps.numToday}
+				</p>
+				<div className="hidden [@media(min-width:480px)]:block mt-2 items-center">
+					<diffObject.icon className={`h-5 w-5 ${diffObject.color}`} />
+					<span className={`pl-0.5 text-xs ${diffObject.color} font-sans`}>
+						{diffObject.sign}
+						{Math.abs(diffObject.percentage).toFixed(1)}%
+					</span>
+					<span className="font-sans text-xs pl-1 whitespace-nowrap">vs yesterday</span>
+				</div>
+			</div>
+			<div className="flex items-center justify-center lg:pr-1 xl:pr-2">
+				<div
+					className={`${dashboardIconProps.color1} px-3 py-5 sm:px-4 sm:py-6 lg:px-1.5 lg:py-3.5 xl:px-4 xl:py-6 rounded-lg`}
+				>
+					<dashboardIconProps.icon
+						className={`h-7 w-7 sm:h-8 sm:w-8 lg:h-10 lg:w-10 ${dashboardIconProps.color2}`}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,12 @@
 import { redirect } from "next/navigation"
 import { createClient } from "../../../../utils/supabase/server"
+import DashboardIcon from "../components/DashboardIcon"
+import {
+	ArrowUpTrayIcon,
+	CreditCardIcon,
+	CurrencyDollarIcon,
+	UserGroupIcon,
+} from "@heroicons/react/24/solid"
 
 export default async function AdminDashboard() {
 	const supabase = await createClient()
@@ -12,7 +19,51 @@ export default async function AdminDashboard() {
 	}
 
 	return (
-		<div>
+		<div className="flex flex-col gap-6">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+				<DashboardIcon
+					icon={ArrowUpTrayIcon}
+					textcolor="text-black"
+					color1="bg-blue-100"
+					color2="text-blue-500"
+					numToday={10}
+					numYesterday={8}
+					title="Leads Delivered Today"
+				/>
+				<DashboardIcon
+					icon={CreditCardIcon}
+					textcolor="text-black"
+					color1="bg-red-100"
+					color2="text-red-500"
+					numToday={10}
+					numYesterday={12}
+					title="Credits Issued Today"
+				/>
+				<DashboardIcon
+					icon={CurrencyDollarIcon}
+					textcolor="text-green-500"
+					color1="bg-green-100"
+					color2="text-green-500"
+					numToday={10}
+					numYesterday={8}
+					title="Net Billable Leads"
+				/>
+				<DashboardIcon
+					icon={UserGroupIcon}
+					textcolor="text-black"
+					color1="bg-purple-100"
+					color2="text-purple-500"
+					numToday={10}
+					numYesterday={6}
+					title="Active Clients"
+				/>
+			</div>
+			<div>
+				<div className="bg-white p-6 rounded shadow col-span-4">Big Box 1</div>
+			</div>
+			<div>
+				<div className="bg-white p-6 rounded shadow col-span-4">Big Box 2</div>
+			</div>
 			<h1>Welcome, {user.email}</h1>
 
 			<form method="POST" action="/signout">
